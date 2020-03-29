c9bd351647bff666cf59e360cc28ab3ec954c98b
Kasey Chang <kschang77@yahoo.com>
2020-03-28 20:30:40 -0700

    20200328 KC Fixed README.md and fixed final bugs in app.js

--- README.md
+++ README.md
@@ -1,11 +1,66 @@
 # weather-dashboard
 
-## Using 
+Project weather-dashboard is a lightweight single-page Javascript app that allow user to query OpenWeatherMap for present day and 5-day forecast of 
+set cities. One start with nothing, and simply enter cities to add it to
+the list of cities. Click on the city name to retrieve data for that city. 
 
-Bootstrap 4
-jQuery
-OpenWeatherMaps
+## Project Repo
 
-# The Rest
+http://github.com/kschang77/weather-dashboard
 
-To be updated
+## Prerequisites
+
+Any Javascript-capable browser
+
+## Installation
+
+No installation required
+
+# Built With 
+
+
+[Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+[Bootstrap 4](https://getbootstrap.com/docs/4.4/getting-started/introduction/)
+
+[jQuery](https://jquery.com/)
+
+[FontAwesome](https://fontawesome.com/)
+
+[OpenWeather](https://openweathermap.org/api)
+
+
+## Code Snippet
+
+        $(".form-inline").on('submit', function(event){
+            event.preventDefault();
+            var tempCity = $("#city").val(); 
+            cityList.unshift(tempCity)
+            console.log(cityList)
+            saveCity();
+            $("#city").val("");
+            refreshCity();
+            queryCurrentWeather(tempCity);
+        })
+
+
+# Deployed Link
+
+https://kschang77.github.io/weather-dashboard/
+
+
+## Author
+
+**Kasey Chang** 
+
+- [Link to Github](https://github.com/kschang77)
+- [Link to LinkedIn](https://www.linkedin.com/in/kasey-chang)
+
+
+## License
+
+This project is licensed under the MIT License 
+
+## Acknowledgments
+
+* Hat tip to Jerome, Kerwin, Mahi, and the UCBEx Coding Bootcamp March 2020 cohort
--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 // consolidate into 3 functions
 
+// body.onload init cityList
+
+//debug
+// var cityList = ["Austin","Chicago","Orlando","San Francisco"];
+
+var cityList = []
+
+// empty the div
+
+function refreshCity () {
+    $("#citydiv").empty();
+
+    // dynamically generate the buttons based on the cityList
+    for (var i = 0; i < cityList.length; i++) {
+        var btnEl = $("<button>").addClass(
+            "btn-city btn btn-outline-primary btn-block text-left"
+        );
+        btnEl.attr("type", "button");
+        btnEl.attr("id", cityList[i]);
+        btnEl.text(cityList[i]);
+        $("#citydiv").append(btnEl);
+    }
+}
+
+function saveCity () {
+    localStorage.setItem("weatherCities", JSON.stringify(cityList))
+}
+
+function loadCity () {
+    var t = localStorage.getItem("weatherCities")
+    if (t != null ) {
+        cityList=JSON.parse(t)
+    }
+}
+
+
 var apikey="8164cdd41308f159d85ff4ef8f3b5171" // openweathermap.org
 var curlat, curlon; // need it for part 2
 
@@ -41,7 +77,7 @@ function queryCurrentWeather(inCity) {
         $("#curdate").text(curdate.toLocaleDateString("en-US"))
 
         var iconweather = res.weather[0].icon; // how to convert that to real icon?
-        console.log(iconweather)
+        // console.log(iconweather)
         $("#iconweather").html("<img src='http://openweathermap.org/img/wn/"+iconweather+"@2x.png' />")
 
         // var curtemp = res.main.temp; // convert from kelvin
@@ -57,22 +93,30 @@ function queryCurrentWeather(inCity) {
         curlat = res.coord.lat;
 
         curlon = res.coord.lon;
+        console.log(curlat+" / "+curlon)
+
+        
+        queryCurrentUV(curlat, curlon);
+        queryForecast(inCity);
     })
 }
 
 
-function queryCurrentUV (inCity) {
+function queryCurrentUV (curlat, curlon) {
     // need to query UV separately
     // api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37
 
+    console.log(curlat + " / " + curlon);
+
+
     var queryurl2 =
-        "api.openweathermap.org/data/2.5/uvi?lat=" + curlat +
+        "https://api.openweathermap.org/data/2.5/uvi?lat=" + curlat +
         "&lon=" + curlon + "&appid=" + apikey;
 
         console.log(queryurl2)
      
     $.ajax({
-        url: queryurl1,
+        url: queryurl2,
         method: "GET"
     }).then(function(res) {
 
@@ -81,8 +125,30 @@ function queryCurrentUV (inCity) {
         var curUV = res.value;
 
     // map to display element and update w/ color
-    });
 
+        $("#curUV").text(curUV);
+        $("#curUV").removeClass();
+
+        $("#curUV").addClass("p-2");
+
+        if (curUV < 3) {
+            $("#curUV").addClass("bg-success");
+        } else if (curUV < 6) {
+            $("#curUV").addClass("bg-warning");
+        } else if (curUV < 8 ) {
+            $("#curUV").addClass("bg-danger text-white");
+        } else if (curUV < 11 ) {
+            $("#curUV").addClass("bg-secondary text-white");
+        } else {
+            $("#curUV").addClass("bg-dark text-white");
+        }
+    // according to EPA, 
+    // <2 is low bg-success
+    // 3-5 is moderate bg-warning (yellow)
+    // 6-7 is high bg-danger 
+    // 8-10 is very high bg-secondary text-white
+    // >11 WTF? bg-dark text-white
+    });
 
 }
 
@@ -91,36 +157,50 @@ function queryForecast (inCity) {
     // api.openweathermap.org/data/2.5/forecast?q=London,us
 
     var queryurl3 =
-    "api.openweathermap.org/data/2.5/forecast?q=" + inCity + 
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + inCity + 
     "&appid=" + apikey;
 
-    console.log(response);
+    console.log(queryurl3)
+     
+    $.ajax({
+        url: queryurl3,
+        method: "GET"
+    }).then(function(res) {
 
-    // res.list[x]
+        console.log(res);
 
-    // must filter as you can get multiple entries per date
+        // for testing, we'll just assume x=0
+        // x = 1 ;
+        for (x=1 ; x<=5 ; x++) {
 
-    var curdate2 = res.list[x].dt_txt; // chop to fit
+            y = 4+(x-1)*8
+            
+            console.log(res.list[y])
+                // must filter as you can get multiple entries per date
 
-    // get icon
+            var curdate2 = res.list[y].dt_txt; // chop to fit
+            // curdate2 = curdate2.substr(0,curdate2.indexOf(' '));
+            var curdate2x = new Date(curdate2)
+            console.log(curdate2x)
+            $(".curdate2.day-"+x).text(curdate2x.toLocaleDateString("en-US"))
+            // get icon
 
-    var curicon2 = res.list[x].weather.icon;
-
-    // get temp
-
-    var curtemp2 = res.list[x].main.temp_max; // convert from kelvin
-
-    // get humidity
-
-    var curhumid2 = res.list[x].main.humidity; //
-
-
-    // variables done, update onscreen elements
-
+            var curicon2 = res.list[y].weather[0].icon;
+        // console.log(iconweather)
+            $(".curicon2.day-"+x).html("<img src='http://openweathermap.org/img/wn/"+curicon2+"@2x.png' />")
 
 
+            // get temp
+            var fahsymbol = "&deg F";
+            var curtemp2 = res.list[y].main.temp; // convert from kelvin
+            $(".curtemp2.day-"+x).html(Math.round(kelvinToFahrenheit(curtemp2)*10)/10+decodeURIComponent(fahsymbol));
+
+            // get humidity
+            var curhumid2 = res.list[y].main.humidity; //
+            $(".curhumid2.day-"+x).text(curhumid2+"%");
+        }
+    })
 }
-
 
 // event handlers
 
@@ -132,15 +212,30 @@ $(document).on("click", ".btn-city", function(event) {
     var curcity = $(this).attr("id") // That gives the city
 
   // queryCurrentWeather
-    queryCurrentWeather(curcity);
+    queryCurrentWeather(curcity);;
 
-
-  // querycurrentUV
-  // queryForecast
 });
 
 // one for the search button 
 // search button adds a button to the #citydiv container populated as a button
 // if the city is valid, but need to know what validation to accept
 
+$(".form-inline").on('submit', function(event){
+    event.preventDefault();
+    var tempCity = $("#city").val(); 
+    cityList.unshift(tempCity)
+    console.log(cityList)
+    saveCity();
+    $("#city").val("");
+    refreshCity();
+    queryCurrentWeather(tempCity);
+})
+
+// init
+loadCity();
+refreshCity();
+if (cityList.length > 0) {
+  queryCurrentWeather(cityList[0]);
+//   $("#citydiv:first-child").addClass("active")
+}
 
